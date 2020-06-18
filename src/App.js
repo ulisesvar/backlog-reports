dab4ff280e69c6b8380d899244f9b7888ae17f87
ulisesvar <ulises.vargas@gmail.com>
2020-06-18 17:23:43 -0500

    try

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     this.state = {
       activities: []
     }
+    
   }
   componentDidMount() {
     fetch('https://ulfix.backlog.com/api/v2/issues?apiKey=1H0ln17z0cb2tGznBxsZRwPbgZldx8WM6LHSS0ngqy451p1kM7KtwmHqvWvQalEC&projectId[]=30508&assigneeId[]=103605&startDateSince=2020-03-01&startDateUntil=2020-03-31&sort=startDate&order=asc&count=100')
